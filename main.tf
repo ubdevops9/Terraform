3e2656b5c0a625e5cac048e7f568ab305d07f877
ubdevops9 <132242433+ubdevops9@users.noreply.github.com>
2024-01-20 13:14:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # Create Instance
 resource "aws_instance" "web" {
-  ami           = "ami-0b2d4cff29fcd27bf"
+  ami           = "ami-008fe2fc65df48dac"
   instance_type = "t3.micro"
 #  key_name = "ubdevops9"
 
